3cfc10aa69082a66bccf4979022bf63064f7f5e6
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-01-09 00:37:04 +0900

    분석

--- react_study/react_project/src/App.js
+++ react_study/react_project/src/App.js
@@ -2,23 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "리액트";
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="react">{name}</div>
   );
 }
 
