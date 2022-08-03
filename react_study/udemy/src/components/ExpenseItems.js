b5536476a4da87c708ed87f9aa7acf593a20141d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-08-03 17:15:06 +0900

    css 추가

--- react_study/udemy/src/components/ExpenseItems.js
+++ react_study/udemy/src/components/ExpenseItems.js
@@ -1,7 +1,13 @@
+import './ExpenseItems.css';
+
 function ExpenseItems() {
   return (
-    <div>
-      <h2>ExpenseItems</h2>
+    <div className="expense-item">
+      <div>8월 3일 2022</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item_price">10000원</div>
+      </div>
     </div>
   );
 }
