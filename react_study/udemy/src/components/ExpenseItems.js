4a8fa95b084305d283a70bd245fa8ad605647ad7
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-08-04 04:14:06 +0900

    컴포넌트 분할

--- react_study/udemy/src/components/ExpenseItems.js
+++ react_study/udemy/src/components/ExpenseItems.js
@@ -1,20 +1,13 @@
 import './ExpenseItems.css';
+import ExpenseDate from "./ExpenseDate"
 
 function ExpenseItems(props) {
-  const expenseDate = props.date;
   const expenseTitle = props.title;
   const ExpenseAmount = props.amount;
-  const month = props.date.toLocaleString("en-US", { month: "long" })
-  const day = props.date.toLocaleString("en-US", { month: "2-digit" });
-  const year = props.date.getFullYear();
-
+  
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item_price">${ExpenseAmount}</div>
