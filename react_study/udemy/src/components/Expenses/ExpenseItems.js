ee7591ce3c532444ebbf46bb61678df87aacea4e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-08-23 22:59:03 +0900

    state

--- react_study/udemy/src/components/Expenses/ExpenseItems.js
+++ react_study/udemy/src/components/Expenses/ExpenseItems.js
@@ -1,18 +1,25 @@
+import React, { useState } from 'react'
+
 import './ExpenseItems.css';
 import ExpenseDate from "./ExpenseDate"
 import Card from "../UI/Card"
 
 const ExpenseItems = (props) => {
-  const expenseTitle = props.title;
-  const ExpenseAmount = props.amount;
+
+  const [title, setTitle] = useState(props.title)
+
+  const clickHandler = () => {
+    setTitle('Updated!')
+  }
   
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item_price">${ExpenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item_price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
